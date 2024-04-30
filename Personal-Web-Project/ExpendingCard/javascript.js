7b87e947950bb5a3467e9ba48f056bf1d101353b
shisui06 <127260275+shisui06@users.noreply.github.com>
2024-04-30 17:40:54 -0400

    mysql data added

---
+++ Personal-Web-Project/ExpendingCard/javascript.js
@@ -0,0 +1,1 @@
+const panels = docuement.querySelector('panel')
---
+++ home project DB.session.sql
@@ -0,0 +1,12 @@
+
+-- @block
+
+create table in
+    id INT PRIMARY key ,
+    adress varchar(4),
+    app_size decimal(4 1/2)
+
+INSERT INTO Fractions (id, value) VALUES (1, 4.5)
+
+
+SELECT TABLE in rent_mtl
