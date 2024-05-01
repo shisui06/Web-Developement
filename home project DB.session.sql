427343f588908d3ba1b736b6eb18f7595fd47a58
shisui06 <127260275+shisui06@users.noreply.github.com>
2024-04-30 20:08:12 -0400

    new directory update

---
+++ home project DB.session.sql
@@ -0,0 +1,15 @@
+
+-- @block
+
+
+
+USE rent_mtl;
+create table in
+    id INT PRIMARY key ,
+    adress varchar(4),
+    app_size decimal(4 1/2)
+
+INSERT INTO Fractions (id, value) VALUES (1, 4.5)
+
+
+SELECT TABLE in rent_mtl
