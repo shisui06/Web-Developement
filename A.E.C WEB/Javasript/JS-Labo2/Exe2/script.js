761e076ec64c11498dd78c54fba0d00d3b953f8d
shisui06 <127260275+shisui06@users.noreply.github.com>
2024-05-10 15:25:40 -0400

    new file
    new file

---
+++ A.E.C WEB/Javasript/JS-Labo2/Exe2/script.js
@@ -0,0 +1,7 @@
+var age;
+age = window.parseInt(window.prompt("Entrez votre âge",""));
+if (age >= 18) {
+	window.alert("Âge plus grand ou égal à 18");
+} else {
+	window.alert("Âge plus petit que 18");
+}
