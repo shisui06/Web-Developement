fdb4366e8d363cd80521454fb2c1979b5a567e81
shisui06 <127260275+shisui06@users.noreply.github.com>
2024-10-16 08:41:16 -0400

    new react project

--- MyProject/React-project/nike-app/src/App.jsx
+++ MyProject/React-project/nike-app/src/App.jsx
@@ -3,31 +3,33 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <nav>
+          <div classname="logo">
+             <img src="/images/brand_logo.png" alt="logo" />
+          </div>
+          <ul>
+            <li>Menu</li>
+            <li>Location</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+          
+          <button>Login</button>
+        </nav>
+       
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+  
+      
+        
+      
+      
     </>
   )
 }
