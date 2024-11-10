7700dc9507e2eeef48235bd59c55fa7432f9c05a
shisui06 <127260275+shisui06@users.noreply.github.com>
2024-11-09 19:12:40 -0500

    new mongodb files

--- A.E.C-WEB/Bloc-3/Initiation-au-developpement-de-microservices-web/mongodb/server.js
+++ A.E.C-WEB/Bloc-3/Initiation-au-developpement-de-microservices-web/mongodb/server.js
@@ -1,8 +1,6 @@
 const { MongoClient } = require('mongodb');
 
-
-const uri = mongodb+srv//apou01:admin@cluster0.afy4tzh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-
+const uri = "mongodb+srv://apou01:admin@cluster0.afy4tzh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client = new MongoClient(uri);
 
 const databaseName = "school";
@@ -14,7 +12,6 @@ async function run() {
         const db = client.db(databaseName);
         const collection = db.collection(collectionName);
 
-        
         const studentsData = [
             { nom: "Pierre", note1: 55 },
             { nom: "Kamba", note1: 80, note2: 95 },
@@ -25,9 +22,44 @@ async function run() {
             { nom: "Etienne", note1: 75, note3: 78 }
         ];
 
-
         await collection.insertMany(studentsData);
         console.log("Données insérées.");
+
+        const allStudents = await collection.find({}).toArray();
+        console.log("Toutes les données:", allStudents);
+
+        
+        await collection.updateMany(
+            { note1: { $exists: false } },
+            { $set: { note1: 50 } }
+        );
+        await collection.updateMany(
+            { note2: { $exists: false } },
+            { $set: { note2: 50 } }
+        );
+        await collection.updateMany(
+            { note3: { $exists: false } },
+            { $set: { note3: 50 } }
+        );
+    
+
+    
+        const studentsWithAverage = await collection.find({}).toArray();
+        studentsWithAverage.forEach(student => {
+            const notes = [student.note1, student.note2, student.note3].filter(n => n !== undefined);
+            const average = notes.length > 0 ? notes.reduce((a, b) => a + b) / notes.length : 0;
+            console.log(`Moyenne de ${student.nom}: ${average}`);
+        });
+
+
+        await collection.deleteMany({
+            $expr: { $lt: [{ $size: { $filter: { input: ["$note1", "$note2", "$note3"], cond: { $gt: ["$$this", null] } } } }, 2] }
+        });
+        
+
+
+        await collection.deleteOne({ nom: "Toufik" });
+    
 
     } catch (err) {
         console.error("Erreur lors de la connexion ou de l'insertion des données:", err);
@@ -36,19 +68,4 @@ async function run() {
     }
 }
 
-run();
-
-
-
-/*
-J'ai esséyer de le faire mais cela m'affiche cette erreur.   
-
-
-ReferenceError: mongodb is not defined
-    at Object.<anonymous> (/Users/tamoor/Documents/GitHub/Web-Developement/A.E.C-WEB/Bloc-3/Initiation-au-developpement-de-microservices-web/mongodb/server.js:4:13)
-    at Module._compile (node:internal/modules/cjs/loader:1469:14)
-    at Module._extensions..js (node:internal/modules/cjs/loader:1548:10)
-    at Module.load (node:internal/modules/cjs/loader:1288:32)
-    at Module._load (node:internal/modules/cjs/loader:1104:12)
-    at Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:174:12)
-    at node:internal/main/run_main_module:28:49
+run().catch(console.error);
