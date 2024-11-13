a05384191b2a8782852bde1845c0e3e16d658c83
shisui06 <127260275+shisui06@users.noreply.github.com>
2024-11-12 21:10:15 -0500

    Delete A.E.C-WEB/Bloc-1/Javascript/Conditions directory

--- A.E.C-WEB/Bloc-1/Javascript/Conditions/Labo1/Exe3c/script.js
+++
@@ -1,8 +0,0 @@
-var age;
-age  = window.parseInt(window.prompt("Entrez votre age",""));
-if ( age >= 18 ) { 
-	window.alert ("Age plus grand ou ");}
-	window.alert ("égal à 18");
-} else 
-	window.alert ("Age inférieur à 18");
-}
--- A.E.C-WEB/Bloc-1/Javascript/Conditions/Labo1/Exe4/script.js
+++
@@ -1,9 +0,0 @@
-let pays;
-pays = window.prompt("Votre pays de résidence ?", "") ;
-if (pays === "Chine") {
-	window.alert("Vous vivez dans le pays le plus peuplé du monde") ;
-} else {
-	window.alert("Vous residez au " + pays + ".");
-}
-
-
