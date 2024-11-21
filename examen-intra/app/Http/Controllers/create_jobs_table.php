bcd253ab06eff92e0b60f3379db1099f5b8542df
shisui06 <127260275+shisui06@users.noreply.github.com>
2024-11-21 16:24:01 -0500

    jnck

--- examen-intra/app/Http/Controllers/create_jobs_table.php
+++ examen-intra/app/Http/Controllers/create_jobs_table.php
@@ -6,9 +6,7 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+    
     public function up(): void
     {
         Schema::create('jobs', function (Blueprint $table) {
@@ -45,9 +43,7 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+    
     public function down(): void
     {
         Schema::dropIfExists('jobs');
