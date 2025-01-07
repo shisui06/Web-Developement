14e8c2cc9a89285f7bedd777c2dbc0f05a2878bc
shisui06 <127260275+shisui06@users.noreply.github.com>
2025-01-07 16:50:00 -0500

    new files

--- A.E.C-WEB/Bloc-4/Test-Debug/Examen /test-github/test/test-cucumber/step_definitions/livraison.steps.js
+++ A.E.C-WEB/Bloc-4/Test-Debug/Examen /test-github/test/test-cucumber/step_definitions/livraison.steps.js
@@ -2,7 +2,7 @@ import { Given, When, Then } from '@cucumber/cucumber';
 import assert from 'assert'; 
 
 import { calculerFrais } from '../../reduction';
- let montantFinal,km,isPremium,calculerFrais;  
+ let montantTotal,km,isPremium,calculerFrais;  
 
 Given('le montant de km pacourue {string}', (montant) => {
   km = parseFloat(montant);
@@ -17,6 +17,6 @@ When('client non premium', () => {
   montantFinal = calculerFrais(km,);
 });
 
-Then('le montant final doit être {string}', (resultatAttendu) => {
-  assert.strictEqual(montantFinal, parseFloat(resultatAttendu));
+Then('le montant total doit être {string}', (resultatAttendu) => {
+  assert.strictEqual(montantTotal, parseFloat(resultatAttendu));
 });
